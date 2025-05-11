636955cd831fb0f86ccb171a9b6f4887575206ca
Ali Nabizadeh <alinabizadeh1913@gmail.com>
2025-05-11 13:23:52 +0330

    debug project

--- src/components/layout/grid/index.tsx
+++ src/components/layout/grid/index.tsx
@@ -8,7 +8,7 @@ interface Point {
 }
 
 const GRID_SPACING = 120; // فاصله خطوط
-const RADIUS = 120; // شعاع تاثیر موس
+const RADIUS = 130; // شعاع تاثیر موس
 const BASE_OPACITY = 0.008;
 const ACTIVE_OPACITY = 0.213;
 const TRANSITION_TIME = 313; // ms
--- src/server/api.ts
+++
@@ -1,7 +0,0 @@
-import apiGet from "@/utils/fetchData";
-
-const fetchPosts = () => {
-  return apiGet("Posts");
-};
-
-export { fetchPosts };
