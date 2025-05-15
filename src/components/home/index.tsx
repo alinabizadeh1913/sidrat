52ca0541db2bd7c337a5417c8591c17a644555ef
Ali Nabizadeh <alinabizadeh1913@gmail.com>
2025-05-16 02:42:38 +0330

    change

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -11,9 +11,9 @@ const HomeComponents = () => {
   return (
     <>
       <section className="block md:hidden">
-        <MysticAura zIndex={35} />
+        <MysticAura zIndex={60} />
       </section>
-      <section className="block md:hidden pb-9 md:pb-0 px-[24px] sm:px-[32px] md:px-[48px] lg:px-[64px] z-[30] bg-[#151616e0] sticky top-0 backdrop-blur-[10px]">
+      <section className="block md:hidden pb-9 md:pb-0 px-[24px] sm:px-[32px] md:px-[48px] lg:px-[64px] z-[55] bg-[#151616e0] sticky top-0 backdrop-blur-[10px]">
         <HomeNavbar setIsMenuOpen={setIsMenuOpen} />
       </section>
       <Header />
